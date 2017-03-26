8fd0c87b20cc032fe1fb0889341bcae4ec5f38c7
Dane Parchment <Dane Parchment>
2017-03-26 17:53:40 -0400

    Added necessary routing components and login components

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,6 +4,10 @@ import Footer from "./Footer";
 import Header from "./Header";
 import AppNavbar from "./AppNavbar"
 import Login from "./Login"
+import SearchPlayer from "./SearchPlayer";
+import SearchTournaments from "./SearchTournaments";
+import HomePage from "./HomePage";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 export default class Layout extends React.Component {
   constructor() {
@@ -20,11 +24,10 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
-
       <AppNavbar></AppNavbar>
-      <Login></Login>
-
-
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/search-players" component={SearchPlayer}></Route>
+      <Route path="/search-tournaments" component={SearchTournaments}></Route>
       </div>
     );
   }
