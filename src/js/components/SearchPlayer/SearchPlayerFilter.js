f09c41f26f5c625fcef89b0ccd29885296f98a5a
Dane Parchment <Dane Parchment>
2017-03-26 17:58:41 -0400

    Added necessary Login and Routing components/libraries

---
+++ src/js/components/SearchPlayer/SearchPlayerFilter.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import { Navbar } from "react-bootstrap"
+import { FormGroup } from "react-bootstrap"
+import { FormControl } from "react-bootstrap"
+import { Button } from "react-bootstrap"
+import { Col } from "react-bootstrap"
+
+
+export default class SearchPlayerFilter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameValue: "",
+      descriptionValue : ""
+    };
+  }
+
+  changeName = (e) => this.setState({nameValue: e.target.value})
+
+  changeDescription = (e) => this.setState({descriptionValue: e.target.value})
+
+  changeFilter = () => this.props.handleFilterUpdate(
+    this.state.nameValue, this.state.descriptionValue)
+
+  render() {
+
+    return (
+      <div>
+      <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand >
+        <a href="#">Search Players</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Navbar.Form>
+        <FormGroup style={{marginRight: "5%", marginLeft: "5%"}}>
+          <FormControl type="text" placeholder="Search name"
+            value={this.state.nameValue} onChange={this.changeName}/>
+        </FormGroup>
+        <FormGroup style={{marginRight: "5%"}}>
+          <FormControl type="text" placeholder="Search description"
+            value={this.state.descriptionValue}
+            onChange={this.changeDescription}/>
+        </FormGroup>
+        {' '}
+        <Button type="submit" onClick={this.changeFilter}>Submit</Button>
+      </Navbar.Form>
+    </Navbar.Collapse>
+  </Navbar>
+      </div>
+    );
+  }
+}
---
+++ src/js/mock-data/PlayerSearchResults.js
@@ -0,0 +1,39 @@
+//Players mock data used for SearchPlayer Component
+
+export let playerSearchResults = [
+
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."},
+      {username: "AlexThyMan", description: "Deus Vult!"},
+      {username: "DanePrettyCoolMan", description: "I guess you could say I'm pretty cool"},
+      {username: "SlyGuySean", description: "Pretty Sly for a Sean guy"},
+      {username: "ItsaMauricle", description: "The best at SSB. Not a miracle."}
+
+
+]
