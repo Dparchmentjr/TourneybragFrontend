bb80a1d3f911381356ef8aa038dec542916601c2
Dane Parchment <Dane Parchment>
2017-03-26 17:51:39 -0400

    Added the Routes and Route Libraries

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import createBrowserHistory from 'history/createBrowserHistory'
 import Layout from "./components/Layout";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+const history = createBrowserHistory();
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+    <Router>
+        <Layout>
+        </Layout>
+    </Router>, app
+);
