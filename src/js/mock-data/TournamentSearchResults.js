3135f260c3505dbed1da5a6ab719c803f5dae718
Palacios95 <apala067@fiu.edu>
2017-03-26 22:00:30 -0400

    Fixed bugs.

---
+++ src/js/mock-data/TournamentSearchResults.js
@@ -0,0 +1,35 @@
+//Players mock data used for SearchPlayer Component
+
+export let tournamentSearchResults = [
+
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"},
+      {name: "BlizzardBrawl2017", organizer: "AlexThyMan", date: "2017-05-26"},
+      {name: "HeroesSummit", organizer: "SlyGuySean", date: "2017-04-22"},
+      {name: "SSBrawl", organizer: "ItsaMauricle", date: "2017-06-13"},
+      {name: "LetsGoCSGO", organizer: "DanePrettyCoolMan", date: "2017-05-26"}
+
+
+]
