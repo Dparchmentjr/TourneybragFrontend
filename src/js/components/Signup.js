a990960a0aba4a3ffe1b5e5be4ef024d30b60598
Dane Parchment <Dane Parchment>
2017-04-03 13:53:26 -0400

    Added signup forms for the player and organizer.

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -6,21 +6,115 @@
  @desc This component handles the controls for the login modal.
  ----------------------------------------------------------------------*/
 import React from "react";
-import {Button, Navbar} from "react-bootstrap";
+import {Button, ControlLabel, FormControl, FormGroup, Modal, Navbar} from "react-bootstrap";
+import {Route, Link} from "react-router-dom";
 export default class Signup extends React.Component {
     constructor() {
         super();
-        this.handleClick = this.handleClick.bind(this);
+        this.handleSignupClick = this.handleSignupClick.bind(this);
+        this.closeChooseSignup = this.closeChooseSignup.bind(this);
+        this.handlePlayerSignup = this.handlePlayerSignup.bind(this);
+        this.handleOrganizerSignup = this.handleOrganizerSignup.bind(this);
+        this.state = {
+            showChooseSignupType: false,
+            showOrganizeSignUp: false,
+            showPlayerSignUp: false
+        }
     }
 
-    handleClick() {
-        alert("We are signing you up for an account!");
+    handleSignupClick() {
+        this.setState({showChooseSignupType: true});
+    }
+    handlePlayerSignup() {
+        this.closeChooseSignup();
+        this.setState({showPlayerSignUp: true});
+    }
+    handleOrganizerSignup() {
+        this.closeChooseSignup();
+        this.setState({showOrganizerSignUp: true});
+    }
+
+    closeChooseSignup() {
+        this.setState({showChooseSignupType: false});
     }
 
     render() {
         return (
             <Navbar.Form pullRight>
-                <Button onClick={this.handleClick} style={styles.signupButton}>Signup. It's Free!</Button>
+                <Button onClick={this.handleSignupClick} style={styles.signupButton}>Signup. It's Free!</Button>
+                <Modal show={this.state.showChooseSignupType} onHide={() => {this.setState({showChooseSignupType: false})}}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Choose your account type</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                            <h2>Player</h2>
+                            <p>Choose a player if just want to play in tournaments and show off your stats!</p>
+                            <Button onClick={this.handlePlayerSignup}>Player</Button>
+                        </div>
+                        <div>
+                            <h2>Organizer</h2>
+                            <p>Choose an organizer if just want to host and create tournaments for players!</p>
+                            <Button onClick={this.handleOrganizerSignup}>Organizer</Button>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+                <Modal show={this.state.showPlayerSignUp} onHide={() => {this.setState({showPlayerSignUp: false})}}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Signup As A Player</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <form>
+                            <FormGroup controlId="player-username">
+                                <ControlLabel>Username:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your username here..."/>
+                            </FormGroup>
+                            <FormGroup controlId="player-password">
+                                <ControlLabel>Password:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your password here..."/>
+                            </FormGroup>
+                            <FormGroup controlId="player-gameplayed">
+                                <ControlLabel>Game Played:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter the main game that you play..."/>
+                            </FormGroup>
+                            <FormGroup controlId="player-maincharacter">
+                                <ControlLabel>Favorite Game Character:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your favorite game player..."/>
+                            </FormGroup>
+                            <Button type="Submit">Sign Up</Button>
+                        </form>
+                    </Modal.Body>
+                </Modal>
+                <Modal show={this.state.showOrganizerSignUp} onHide={() => {this.setState({showOrganizerSignUp: false})}}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Signup As An Organizer</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <form>
+                            <FormGroup controlId="player-username">
+                                <ControlLabel>Username:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your username here..."/>
+                            </FormGroup>
+                            <FormGroup controlId="player-password">
+                                <ControlLabel>Password:</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your password here..."/>
+                            </FormGroup>
+                            <Button type="Submit">Sign Up</Button>
+                        </form>
+                    </Modal.Body>
+                </Modal>
             </Navbar.Form>
         );
     }
